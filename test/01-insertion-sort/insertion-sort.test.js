2db451e91a00a9b5a704ec8deb195e1fa7c74f36
Carlos A. Estrada <carlos.estrada@coretechnologies.io>
2021-10-14 04:50:23 -0400

    Modify test to use Jest

--- test/01-insertion-sort/insertion-sort.test.js
+++ test/01-insertion-sort/insertion-sort.test.js
@@ -1,6 +1,3 @@
-const chai = require("chai");
-const expect = chai.expect;
-
 function insertionSort(nums) {
   for (let i = 1; i < nums.length; i++) {
     for (let j = i - 1; j >= 0; j--) {
@@ -16,10 +13,8 @@ function insertionSort(nums) {
 
 // unit tests
 // do not modify the below code
-describe("Insertion Sort", function () {
-  it("Should be sorted by insertionSort function", () => {
-    const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-    insertionSort(nums);
-    expect(nums).to.have.ordered.members([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-  });
+test("insertion sort", function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  insertionSort(nums);
+  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
